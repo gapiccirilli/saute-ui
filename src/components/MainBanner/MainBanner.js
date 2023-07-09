64eb2e77b8f3d51302cdae24f8800145c494fef5
Gino Piccirilli <gapiccirilli@outlook.com>
2023-07-09 15:44:56 -0400

    create and style starting banner

--- src/components/MainBanner/MainBanner.js
+++ src/components/MainBanner/MainBanner.js
@@ -1,9 +1,10 @@
+import styles from "./MainBanner.module.css";
 import Logo from "../Logo/Logo";
 
 
 function MainBanner() {
     return (
-        <div>
+        <div className={styles.banner}>
             <Logo />
         </div>
     );

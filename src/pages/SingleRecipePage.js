96a9fdcc2d89117ea5c4be346695abd8bcec71af
Gino Piccirilli <gapiccirilli@outlook.com>
2023-07-10 11:46:27 -0400

    add useEffect to SingleRecipePage

--- src/pages/SingleRecipePage.js
+++ src/pages/SingleRecipePage.js
@@ -1,26 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import RecipeCard from "../components/Cards/RecipeCard";
-import AddNewCard from "../components/Cards/AddNewCard";
 import ErrorMessage from "../components/Error/ErrorMessage";
 
 function SingleRecipePage() {
     const { recipeId } = useParams();
-    const [recipe, setRecipe] = useState({});
+    const [{recipeName, description}, setRecipe] = useState({});
     const [items, setItems] = useState([]);
     const [error, setError] = useState("");
 
     useEffect(() => {
         async function getItems() {
-            
+            try {
+                const response = await fetch(`http://localhost:8080/api/recipes/${recipeId}/items/multiple`);
+                
+                if (!response.ok) {
+                    const errorMessage = await response.json();
+                    throw new Error(errorMessage.message);
+                }
+                
+                const data = await response.json();
+                setItems(data);
+            } catch(err) {
+                const {message} = err;
+                setError(message);
+            }
         }
         getItems();
-    }, []);
+    }, [recipeId]);
     
     
     return (
         <div>
-            <div>RECIPE</div>
+            <div>
+                <h1>{recipeName}</h1>
+                <h6>{description}</h6>
+            </div>
             {error && <ErrorMessage message={error}/>}
         </div>
     );

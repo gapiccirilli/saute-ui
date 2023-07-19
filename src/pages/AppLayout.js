b5dfdd09ca270ea64142fca6a98ea832b3154441
Gino Piccirilli <gapiccirilli@outlook.com>
2023-07-19 12:47:25 -0400

    add more styling

--- src/pages/AppLayout.js
+++ src/pages/AppLayout.js
@@ -4,7 +4,7 @@ import Header from "../components/Header/Header";
 
 function AppLayout() {
     return (
-        <div>
+        <div className="content">
             <Header />
             <Outlet />
         </div>

725c1d59465c1bcde8c29992d6b7bb5a78fa9a47
Gino Piccirilli <gapiccirilli@outlook.com>
2023-07-20 17:34:23 -0400

    fix page container overflow issue

--- src/test-data/Data.js
+++ src/test-data/Data.js
@@ -12,40 +12,40 @@ export const testRecipeBooks = [
         "id" : 3,
         "recipeBookName" : "Italian Recipes"
     },
-    {
-        "id" : 4,
-        "recipeBookName" : "Croatian Recipes"
-    },
-    {
-        "id" : 5,
-        "recipeBookName" : "English Recipes"
-    },
-    {
-        "id" : 6,
-        "recipeBookName" : "Gluten Free Recipes"
-    },
-    {
-        "id" : 7,
-        "recipeBookName" : "Dessert Recipes"
-    },
-    {
-        "id" : 8,
-        "recipeBookName" : "Mexican Recipes"
-    },
-    {
-        "id" : 9,
-        "recipeBookName" : "Breakfast Recipes"
-    },
-    {
-        "id" : 10,
-        "recipeBookName" : "Meat Recipes"
-    },
-    {
-        "id" : 11,
-        "recipeBookName" : "Dairy Free Recipes"
-    },
-    {
-        "id" : 12,
-        "recipeBookName" : "Dairy Free Recipes"
-    }
+    // {
+    //     "id" : 4,
+    //     "recipeBookName" : "Croatian Recipes"
+    // },
+    // {
+    //     "id" : 5,
+    //     "recipeBookName" : "English Recipes"
+    // },
+    // {
+    //     "id" : 6,
+    //     "recipeBookName" : "Gluten Free Recipes"
+    // },
+    // {
+    //     "id" : 7,
+    //     "recipeBookName" : "Dessert Recipes"
+    // },
+    // {
+    //     "id" : 8,
+    //     "recipeBookName" : "Mexican Recipes"
+    // },
+    // {
+    //     "id" : 9,
+    //     "recipeBookName" : "Breakfast Recipes"
+    // },
+    // {
+    //     "id" : 10,
+    //     "recipeBookName" : "Meat Recipes"
+    // },
+    // {
+    //     "id" : 11,
+    //     "recipeBookName" : "Dairy Free Recipes"
+    // },
+    // {
+    //     "id" : 12,
+    //     "recipeBookName" : "Dairy Free Recipes"
+    // }
 ];
